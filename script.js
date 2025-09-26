f0d0f2d833ef27dd6d09c906347451e738b6eb08
kalyanikharat <132137407+kalyanikharat@users.noreply.github.com>
2025-09-26 11:39:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.getElementById("removeBtn").onclick = function () {
+  let dropdown = document.getElementById("colorSelect");
+  dropdown.remove(dropdown.selectedIndex);
+};
+
